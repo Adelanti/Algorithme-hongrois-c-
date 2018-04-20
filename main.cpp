5e1ea2cb685694d5b80e85ef4b10bc0c72966ede
Adelanti <37774563+Adelanti@users.noreply.github.com>
2018-04-20 23:13:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-
+#include<stdlib.h>
+#include<iostream>
 #include<stdio.h>
 #include<conio.h>
 #define max 20    
@@ -6,34 +7,172 @@
        Date :17/04/18
    */    
    	  	
-   
-    int main()
-    {
-	  int tr,size,m,av,i,j,k,r,lg[max],ln[max],lk[max],c,li,co,diff,l=0,lz[max],cz[max],ze[max],zr[max]
-        ,a[max][max],t[max][max],ad[max][max],minr[max],minc[max],ct[max][max],minnew;
-        int cross[max][max],alloccount=0,minrow,mincol;
-        int nbz[max],nrc[max],ncc[max],nl=0;
-        int fzr[max][max],fzc[max][max];
-        int fz[max][max],flag=0,alloc[max][max];
-        int ffz[max][max],fffz[max][max];
-        int cost[max][max],costm=0;
-      
-        //inputting array
-        printf("*******************La taille de la matrice******************** \n ");
-        printf("Donner le nombre de ligne : ");
-        scanf("%d",&r);
-        printf("Donner le nombre de colonne : ");
-        scanf("%d",&c);
-        //inputting array
-        for(i=0;i<r;i++)
+   	//################################**Le corps des fonctions **############################  	
+
+int mark_line(int mat[][max],int j,int lg[max],int size)
+{
+	int i,ln[max];
+	
+             
+    		
+    return 	ln[j];	
+}
+
+
+int zero_encadre(int mat[][max],int size)
+{
+	int i,j,l;
+	for(i=0;i<size;i++)
         {
-            for(j=0;j<c;j++)
-            {   
-			printf("Entrer l'element a[%d][%d] : ",i,j);
-            scanf("%d",&a[i][j]);
+            for(j=0;j<size;j++)
+            {
+                   if(mat[i][j]==-1)
+                   {
+                    l=l+1;
+                   }
             }
         }
-        if(r!=c)
+        return l;
+}
+
+
+
+void mark_zero_line(int mat[][max],int lz[max],int k,int size)
+{
+	int i,j,co;
+	if(lz[k]==1)
+			   {
+			 	for(j=0;j<size;j++)
+			 	 {
+			    	if (mat[k][j]==0)
+			  	     { 
+					   co=j;
+			  		    mat[k][j]=-1 ;
+			  	     }
+			     }
+			  	for(i=0;i<size;i++)
+			  	 {
+			  	   if (mat[i][co]==0)
+					{
+					  mat[i][co]=-2;
+					  lz[i]=lz[i]-1;
+			  	  	}
+			     }
+			  }
+}	  	
+ 
+void mark_zero_col(int mat[][max],int cz[max],int m,int size)
+{
+	int i,j,li;
+	if(cz[m]==1)
+			 {
+			 	for(i=0;i<size;i++)
+			 	{
+			  	 if (mat[i][m]==0)
+			  	  { 
+					li=i;
+			  		mat[i][m]=-1 ;
+			  	  }
+			    }
+			  	for(j=0;j<size;j++)
+			  	{
+			  		if (mat[li][j]==0)
+					{
+					  	mat[li][j]=-2;
+					  	cz[j]=cz[j]-1;
+					}
+				}	
+			 }
+}	   
+   	  	
+   	  	
+int min_col(int mat[][max],int j,int size)
+{
+    int aug,aug1,i,minc[max];
+
+      
+            minc[j]=mat[0][j];
+            for(i=0;i<size;i++)
+            {
+                if(minc[j]>mat[i][j])
+                {
+                    minc[j]=mat[i][j];
+                }
+            }
+        
+return minc[j];
+}   	  	
+
+
+
+int min_row (int mat[][max],int i,int size)
+{
+	int j,minr[max];
+	
+	
+            minr[i]=mat[i][0];
+            for(j=0;j<size;j++)
+            {
+                if(minr[i]>mat[i][j])
+                {
+                    minr[i]=mat[i][j];
+                }
+            }
+        
+	return minr[i] ;
+	
+}
+
+	  	
+int make_mat(int mat[][max],int matt[][max],int size)   	  	
+{
+	int i,j;
+for(i=0;i<size;i++)
+   {
+    for(j=0;j<size;j++)
+      {
+        mat[i][j]=matt[i][j];
+      }
+    }			 			 
+}   	  	
+ 	  	
+   	  	
+int rech_min_pos(int mat[][max],int size)
+{
+	 int min,a,b;
+	 min=mat[0][0];  
+  for(a=0;a<size;a++)
+    {
+     for(b=0;b<size;b++)
+      	{
+		 if(mat[a][b] > 0 && mat[a][b] < min )
+		   {
+		    min = mat[a][b];
+			}
+    	}
+    }
+    return min ;	
+} 	
+   	  	
+   	  	
+void affich_mat(int mat[][max],int size)
+ {
+ 	int a,b;
+ 	for(a=0;a<size;a++)
+        {
+            for(b=0;b<size;b++)
+            {
+                printf("| %d ", mat[a][b]);
+            }
+            printf("\n");
+        }
+ }  	  	
+   	  	
+int mat_size(int r,int c,int mat[][max])
+{
+	int diff,i,j,size;
+
+	   if(r!=c)
         {
             if(r>c)
             {
@@ -42,72 +181,100 @@
                 {
                     for(i=0;i<r;i++)
                     {
-                        a[i][c]=0;
+                        mat[i][c]=0;
                     }
                     c++;
 					diff--;
                 }
+              size=r;  
             }
-            if(r<c)
+          if(r<c)
             {
                 diff=c-r;
                 while(diff!=0)
                 {
                     for(i=0;i<c;i++)
                     {
-                        a[r][i]=0;
+                        mat[r][i]=0;
                     }
                     r++;diff--;
                 }
+              size=r;  
             }
         }
         if(r==c)
         {
             size=r;
         }
-        //displaying array
-        printf(" \n La matrice est : \n");
-        for(i=0;i<size;i++)
+	
+	return size ;
+}  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
+int main()
+    {
+	  int tr,size,aug,aug1,m,av,i,j,k,r,lg[max],ln[max],c,li,co,diff,l=0,lz[max],cz[max]
+        ,a[max][max],t[max][max],ad[max][max],minr[max],minc[max],minnew;
+        int cross[max][max];
+        int nbz[max],nrc[max],ncc[max];
+        int cost[max][max],costm=0;
+      
+        
+        // saisisser la taille de la matrice
+        printf("*******************La taille de la matrice******************** \n ");
+        printf("Donner le nombre de ligne : ");
+        scanf("%d",&r);
+        printf("Donner le nombre de colonne : ");
+        scanf("%d",&c);
+        
+        //saisisser les eléments de la matrice 
+        for(i=0;i<r;i++)
         {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ", a[i][j]);
+            for(j=0;j<c;j++)
+            {   
+			printf("Entrer l'element a[%d][%d] : ",i,j);
+            scanf("%d",&a[i][j]);
             }
-            printf("\n");
         }
         
-        //initializing all matrices
+        //obtenir la taille de matrice 
+        size=mat_size(r,c,a);
+        
+        //affichage de la matrice 
+        printf(" \n La matrice est : \n");
+        affich_mat(a,size);
+        
+        //initialiser toutes les matrices
         for(i=0;i<size;i++)
         {
             lz[i]=cz[i]=0;
             nbz[i]=ncc[i]=0;
             for(j=0;j<size;j++)
             {
-                fzr[i][j]=fzc[i][j]=fz[i][j]=ffz[i][j]=fffz[i][j]=0;
-                alloc[i][j]=0;cost[i][j]=0;
+                cost[i][j]=0;
             }
         }
         //making cost matrix
+        make_mat(cost,a,size);
         
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                cost[i][j]=a[i][j];
-            }
-        }
         //calculating min row
         for(i=0;i<size;i++)
         {
-            minr[i]=a[i][0];
-            for(j=0;j<size;j++)
-            {
-                if(minr[i]>a[i][j])
-                {
-                    minr[i]=a[i][j];
-                }
-            }
-        }
+       		minr[i]=min_row(a,i,size);
+    	}
         //subtracting min row from respective row
         for(i=0;i<size;i++)
         {
@@ -116,18 +283,14 @@
                 a[i][j]=a[i][j]-minr[i];
             }
         }
-        //calculating min column
-        for(i=0;i<size;i++)
-        {
-            minc[i]=a[0][i];
-            for(j=0;j<size;j++)
-            {
-                if(minc[i]>a[j][i])
-                {
-                    minc[i]=a[j][i];
-                }
-            }
-        }
+        
+        //calculating min col
+        for(j=0;j<size;j++)
+          {
+    		minc[j]=min_col(a,j,size);
+		   }
+		   
+		   
         //subtracting min column from respective column
         for(i=0;i<size;i++)
         {
@@ -136,127 +299,68 @@
                 a[j][i]=a[j][i]-minc[i];
             }
         }
+        
         //displaying reduced matrix
         printf("\n La matrice reduite est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-            printf("| %d ", a[i][j]);
-            }
-            printf("\n");
-        }
+        affich_mat(a,size);
+        
         //copying reduced matrix
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                t[i][j]=a[i][j];
-                cross[i][j]=a[i][j];
-                ad[i][j]=a[i][j];
-                //t[i][j] is row column reduced matrix
-     
-            }
-        }
+        make_mat(t,a,size);
+        make_mat(cross,a,size);
+        make_mat(ad,a,size);
+        
         //calculating row zero i.e. no of zeros in row
         for(i=0;i<size;i++)
         {
-            for(j=0;j<size;j++)
-            {
-                if(t[i][j]==0)
-                {
-                    lz[i]=lz[i]+1;
-                    
-                } 
-				  
-            }
+         for(j=0;j<size;j++)
+    		{
+     			if(t[i][j]==0)
+      				{
+       				lz[i]=lz[i]+1;             
+      				}			  
+     		}	   
         }
-            //striking lines in row
+        
+        //striking lines in row
+            
             k=0;
             while(k<size)
             {
-			  if(lz[k]==1)
-			   {
-			 	for(j=0;j<size;j++)
-			 	 {
-			    	if (a[k][j]==0)
-			  	     { 
-					   co=j;
-			  		    a[k][j]=-1 ;
-			  	     }
-			     }
-			  	for(i=0;i<size;i++)
-			  	 {
-			  	   if (a[i][co]==0)
-					{
-					  a[i][co]=-2;
-					  lz[i]=lz[i]-1;
-			  	  	}
-			     }
-			  }
-			   
+			mark_zero_line(a,lz,k,size);
 			 k++;	
 			}	
+			
         //calculating colomn zero
+        
         for(j=0;j<size;j++)
-        {
-            for(i=0;i<size;i++)
+        {	
+         for(i=0;i<size;i++)
             {
-                if(a[i][j]==0)
+                if(t[i][j]==0)
                 {
                     cz[j]=cz[j]+1;
                 }
-            }	  
+            }	     
         }
+        
         //striking lins in colomn
-         m=0;
+         	m=0;
             while(m<size)
             {
-			 if(cz[m]==1)
-			 {
-			 	for(i=0;i<size;i++)
-			 	{
-			  	 if (a[i][m]==0)
-			  	  { 
-					li=i;
-			  		a[i][m]=-1 ;
-			  	  }
-			    }
-			  	for(j=0;j<size;j++)
-			  	{
-			  		if (a[li][j]==0)
-					{
-					  	a[li][j]=-2;
-					  	cz[j]=cz[j]-1;
-					}
-				}	
-			 }
+			 mark_zero_col(a,cz,m,size);
 			 m++;	
 			}	
         
         l=0;    
-        //calculating no of rows and columns crossed
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                   if(a[i][j]==-1)
-                   {
-                    l=l+1;
-                   }
-            }
-        }
-        printf("\n Nombre de zero marquee : %d \n",l);
+        
+        //calculating no of zero encadre
+       	zero_encadre(a,size);
+      	
+        printf("\n Nombre de zero encadre : %d \n",l);
+        
         //cross matrix
         printf(" \n La matrice de marquage est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",a[i][j]);
-            }
-            printf("\n");
-        }
+        affich_mat(a,size);
         if (l==size)
         {
          for(i=0;i<size;i++)
@@ -274,9 +378,10 @@
         
         printf("\n Le cout minimum d'affectation est : %d ",costm);
     	}
-    	
-    	if(l!=size)//if no of line striked is not equal to the size
-    	{
+    //********************************************************************************************************
+		
+    if(l!=size)       //if no of line striked is not equal to the size
+       { 
     	 for(i=0;i<size;i++)
             {
               for(j=0;j<size;j++)
@@ -306,9 +411,21 @@
             {
             	printf("\n la valeur de ligne %d est : %d \n",i,lg[i]);
 			}
-    	  for(j=0;j<size;j++)
+			
+//""""""""""""""""""""""""""""""""La prtie repetitive """"""""""""""""""""""""""""""""""""""""""""""""""""			
+
+while(l!=size)
+{
+
+
+
+
+
+
+		//Marquer une colonne ayant zero baree sur une ligne marquee			
+    	for(j=0;j<size;j++)
             {
-             for(i=0;i<size;i++)
+			for(i=0;i<size;i++)
                {	
               	 if(a[i][j]==-2 && lg[i]==-11)
 				   {
@@ -316,8 +433,9 @@
 				   	printf("\n La colomne %d est marquee ",j);
 					}	
 			   }
-    		}
-    	  
+			}
+		
+    	  //Marquer tt ligne ayant zero encadre sur une colonne marquee
     	  for(i=0;i<size;i++)
             {
              for(j=0;j<size;j++)
@@ -329,16 +447,13 @@
 					}	
 			   }
     		}
-    	  for(i=0;i<size;i++)
-            {
-             for(j=0;j<size;j++)
-               {	
-				cross[i][j]=t[i][j];
-						
-				}	  
-    		}
-    	  
-    	  
+    	
+
+
+    	//copie de la matrice t sur cross	
+    	make_mat(cross,t,size);
+    	 
+		//  
     	for(i=0;i<size;i++)
             {
              for(j=0;j<size;j++)
@@ -349,7 +464,7 @@
 					}	
 				}	  
     		}
-    		
+    	//	
     		for(j=0;j<size;j++)
             {
              for(i=0;i<size;i++)
@@ -361,29 +476,14 @@
 				}	  
     		}
     	  
-    	  printf(" \n La sous matrice reduite est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",cross[i][j]);
-            }
-            printf("\n");
-        }	
-    	 
-		 
-		  
-		minnew=cross[0][0];	// recherche new min    
-    	for(i=0;i<size;i++)
-            {
-			  for(j=0;j<size;j++)
-            	{
-				  if(cross[i][j] > 0 && cross[i][j] < minnew )
-				  	{
-				  	  minnew = cross[i][j];
-					}
-    		    }
-    	    }
+    	printf(" \n La sous matrice reduite est : \n");
+        affich_mat(cross,size);
+            
+        
+ 		// recherche new min    
+ 		
+ 		
+    	minnew=rech_min_pos(cross,size);
     	printf("\n Le new min est : %d ",minnew);
 			
     	for(i=0;i<size;i++) // retrancher le min from la sous matrice
@@ -398,23 +498,9 @@
     		}
     		
     	printf(" \n La matrice aprés retancher est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",cross[i][j]);
-            }
-            printf("\n");
-        }		
+        affich_mat(cross,size);	
     	printf(" \n La matrice reduite : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",a[i][j]);
-            }
-            printf("\n");
-        }	
+        affich_mat(a,size);
         printf("************************");
     	for(i=0;i<size;i++)
         {
@@ -459,14 +545,7 @@
             }  
         }	
 		printf(" \n La derniere matrice reduite est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",cross[i][j]);
-            }
-            printf("\n");
-        }		
+        affich_mat(cross,size);	
 			
 		for(i=0;i<size;i++)//initialiser conteur 
             {
@@ -476,14 +555,13 @@
 		//calculating new row zero 
         for(i=0;i<size;i++)
         {
-            for(j=0;j<size;j++)
-            {
-                if(cross[i][j]==0)
-                {
-                    lz[i]=lz[i]+1;
-                    
-                } 	  
-            }
+           for(j=0;j<size;j++)
+    		{
+     		if(cross[i][j]==0)
+      			{
+       			lz[i]=lz[i]+1;             
+      			}						  
+     		}	
         }
         
         for(i=0;i<size;i++)
@@ -493,34 +571,14 @@
             
         }		
         
-        
-            //striking lines in row
-            k=0;
+        //striking lines in row
             
+            k=0;
             while(k<size)
             {
-			  if(lz[k]==1)
-			   {
-			 	for(j=0;j<size;j++)
-			 	 {
-			    	if (cross[k][j]==0)
-			  	     { 
-					   co=j;
-			  		    cross[k][j]=-1 ;
-			  	     }
-			     }
-			  	for(i=0;i<size;i++)
-			  	 {
-			  	   if (cross[i][co]==0)
-					{
-					  cross[i][co]=-2;
-					  lz[i]=lz[i]-1;
-			  	  	}
-			     }
-			  }
-			   
+			mark_zero_line(cross,lz,k,size);
 			 k++;	
-			}
+			}	
 			
 			for(j=0;j<size;j++)//initialiser conteur 
             {
@@ -547,54 +605,24 @@
         }
         
         //striking lins in colomn
-         m=0;
+         	m=0;
             while(m<size)
             {
-			 if(cz[m]==1)
-			 {
-			 	for(i=0;i<size;i++)
-			 	{
-			  	 if (cross[i][m]==0)
-			  	  { 
-					li=i;
-			  		cross[i][m]=-1 ;
-			  	  }
-			    }
-			  	for(j=0;j<size;j++)
-			  	{
-			  		if (cross[li][j]==0)
-					{
-					  	cross[li][j]=-2;
-					  	cz[j]=cz[j]-1;
-					}
-				}	
-			 }
+			 mark_zero_col(cross,cz,m,size);
 			 m++;	
 			}	
         
         l=0;    
+        
         //calculating no of rows and columns crossed
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                   if(cross[i][j]==-1)
-                   {
-                    l=l+1;
-                   }
-            }
-        }
+        l=zero_encadre(cross,size);
+        
         printf("\n Nombre de zero marquee : %d \n",l);
+        
         //cross matrix
         printf(" \n La matrice de marquage est : \n");
-        for(i=0;i<size;i++)
-        {
-            for(j=0;j<size;j++)
-            {
-                printf("| %d ",cross[i][j]);
-            }
-            printf("\n");
-        }
+        affich_mat(cross,size);
+        
         costm=0;
         if (l==size)
         { 	
@@ -615,11 +643,10 @@
         
     	}			
 			
-	
-	
-	
     		
 	}
+}
+
 }
 
     	
